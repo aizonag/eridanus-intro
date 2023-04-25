07c742436e33ba47d069e9b4010ce043b95a231a
aizonag <giovana.s.aizona@gmail.com>
2023-04-25 12:33:43 -0500

    lesson 6-2

--- js/index.js
+++ js/index.js
@@ -2,14 +2,15 @@ let footer = document.querySelector('#footer');
 let today = new Date();
 let currentYear = today.getFullYear();
 let copyright = document.createElement("P");
-copyright.innerHTML = "Giovana Aizona " + currentYear;
+copyright.innerHTML = "&copy" + "Giovana Aizona " + currentYear;
 footer.appendChild(copyright);
 
-let skills = [ 
+let skills = [
 "Interpersonal skills",
- "Self-awareness", 
- "Emotional intelligence", 
+ "Self-awareness",
+ "Emotional intelligence",
  "Critical thinking"];
+
 let skillsSection = document.getElementById("skills");
 let skillsList = skillsSection.querySelector("ul");
 
@@ -20,52 +21,51 @@ for(let i = 0; i < skills.length; i++) {
 };
 const messageForm = document.forms.leave_message;
 messageForm.addEventListener ('submit', function(event){
-    event.preventDefault ();
-    
-
+    event.preventDefault();
    let usersName = event.target.usersName.value;
    let usersemail = event.target.usersEmail.value;
    let usersmessage = event.target.usersMessage.value;
-    
-   const messageSection = document.getElementById ("messages");
-   const messageList = messageSection.querySelector ("ul");
-   let newMessage = document.createElement ("li");
+
+   const messageSection = document.getElementById("messages");
+   const messageList = messageSection.querySelector("ul");
+   let newMessage = document.createElement("li");
 
    newMessage.innerHTML =
-   `<a href = "mailito: ${usersemail}">${usersName}</a>
+   `<a href = "mailto: ${usersemail}">${usersName}</a>
    <span>says: ${usersmessage}</span>`;
-   console.log (newMessage);
-   messageList.appendChild (newMessage);
-   messageForm.reset ();
-   
+
+   console.log(newMessage);
+   messageList.appendChild(newMessage);
+
+   let removeButton = document.createElement("button")
+   removeButton.innerText = "Remove";
+   newMessage.appendChild(removeButton);
+   removeButton.addEventListener('click', (event) => {
+	const entry = event.target.parentNode;
+	entry.remove();
+   });
+
+   messageForm.reset();
+
 })
 
-let removeButton = document.getElementById ("messages")
-removeButton.addEventListener ('click', function (event){
-    let button = event.target;
-    debugger;
-    let entry = button.parentNode;
-    entry.remove ();
-    //removeButton.appendChild (newMessage);
-   //newMessage.appendChild (messageList);
-})
 
 fetch('https://api.github.com/users/aizonag/repos')
 .then ((response) =>{
    return response.json();
 })
 
-.then ((response)=> { 
+.then ((response)=> {
     let repositories = response;
     console.log (repositories);
 
     let projectSection = document.getElementById ("projects");
     console.log (projectSection);
-    let projectList = projectSection.querySelector ("ul"); 
+    let projectList = projectSection.querySelector ("ul");
     console.log (projectList);
 
     for(let i = 0; i < repositories.length; i++) {
-        let project = document.createElement("li") 
+        let project = document.createElement("li")
         project.innerText = repositories[i].name
         projectList.appendChild(project)
     }})
